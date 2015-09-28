492f8345b5c0ac13e8404eff229ec47d3be50830
Darth Vader <dvader@deathstar.com>
2015-09-28 14:24:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9c457689cb3e38dc2afa5ae4774d00f9c72721de0b588ca7276993df271ad2cc163d76e645423ec5e5ed01e8921c0f395227befdae0eb3b96f0a928860e7bbd0'
